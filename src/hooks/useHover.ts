d380038631669a498857f14743d79056ed8bf7a2
PRANTA Dutta <prantadutta1997@gmail.com>
2022-01-14 23:44:06 +0600

    uploaded to npm

--- src/hooks/useHover.ts
+++ src/hooks/useHover.ts
@@ -1,11 +1,14 @@
 import { RefObject, useEffect, useState } from 'react'
 
-export function useHover<T extends HTMLElement = HTMLElement>(elementRef: RefObject<T>): boolean {
+export function useHover<T extends HTMLElement = HTMLElement>(
+  elementRef: RefObject<T>
+): boolean {
   const [value, setValue] = useState<boolean>(false)
 
   const handleMouseEnter = () => setValue(true)
   const handleMouseLeave = () => setValue(false)
 
+  // @ts-ignore
   useEffect(() => {
     const node = elementRef?.current
 
